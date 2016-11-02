2721c7b3c451e7fcef0d4599db47de4124b2f4b9
Parth Jansari <parthjansari@outlook.com>
2016-11-02 21:27:40 +0530

    added work draging

--- _assets/_javascripts/app.js
+++ _assets/_javascripts/app.js
@@ -1,3 +1,4 @@
+counter = 0
 $(".nav-toggle").click(function(event) {
     if ($("nav").hasClass('open')) {
         nav_close()
@@ -7,7 +8,41 @@ $(".nav-toggle").click(function(event) {
     $("nav ul li").click(function(event){
       nav_close()
     });
+    $(".overlay").click(function(event){
+      nav_close()
+    });
 });
+var gridWidth = 520;
+// Draggable.create(".cards", {type:"x", edgeResistance:0.25, bounds:".container", throwProps:true});
+Draggable.create(".cards", {
+    type:"x",
+    edgeResistance:0.8,
+     bounds:{minX:0, maxX:-1880},
+    throwProps:true,
+    snap:function(endValue) {
+      // console.log(endValue);
+      var val = (Math.round(endValue / gridWidth) * gridWidth) - (Math.round(endValue / gridWidth) * 120)
+console.log(val)
+      if (val <=-1200) {
+        return -1200;
+      }
+      else {
+        return val;
+      }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -18,6 +53,7 @@ $(".nav-toggle").click(function(event) {
 
 function nav_open() {
     var tl = new TimelineMax
+    tl.set(".overlay", {display:"block"});
     tl.to("ul.links", 0.2, {
         right: 0
     });
@@ -29,6 +65,12 @@ function nav_open() {
     }, "=-0.2");
     tl.to(".nav-toggle .bar.three", 0.2, {
         transform: "rotate(45deg) translateY(-8px)"
+    }, "=-0.2");
+    tl.to(".cards", 0.2, {
+        left: "500px"
+    }, "=-0.2");
+    tl.to(".overlay", 0.2, {
+        opacity: 0.4
     }, "=-0.2");
     $("nav").addClass('open')
 }
@@ -46,5 +88,12 @@ function nav_close() {
     tl.to(".nav-toggle .bar.three", 0.2, {
         transform: "rotate(0) translateY(0)"
     }, "=-0.2");
+    tl.to(".cards", 0.2, {
+        left: "0"
+    }, "=-0.2");
+    tl.to(".overlay", 0.4, {
+      opacity: 0
+    }, "=-0.2");
+    tl.set(".overlay", {display:"none"});
     $("nav").removeClass('open')
 }
---
+++ _assets/_javascripts/test.js
@@ -0,0 +1,24 @@
+o = function(a) {
+    for (var b = -1 !== (window ? window.location.href : "")
+            .indexOf(String.fromCharCode(103, 114, 101, 101, 110, 115, 111, 99, 107)) && -1 !==
+            a.indexOf(String.fromCharCode(108, 111, 99, 97, 108, 104, 111, 115, 116)),
+            c = [m, String.fromCharCode(99, 111, 100, 101, 112, 101, 110, 46, 105, 111),
+                String.fromCharCode(99, 111, 100, 101, 112, 101, 110, 46, 100, 101, 118),
+                String.fromCharCode(99, 115, 115, 45, 116, 114, 105, 99, 107, 115, 46, 99, 111, 109),
+                String.fromCharCode(99, 100, 112, 110, 46, 105, 111),
+                String.fromCharCode(103, 97, 110, 110, 111, 110, 46, 116, 118),
+                String.fromCharCode(99, 111, 100, 101, 99, 97, 110, 121, 111, 110, 46, 110, 101, 116),
+                String.fromCharCode(116, 104, 101, 109, 101, 102, 111, 114, 101, 115, 116, 46, 110, 101, 116),
+                String.fromCharCode(99, 101, 114, 101, 98, 114, 97, 120, 46, 99, 111, 46, 117, 107),
+                String.fromCharCode(116, 121, 109, 112, 97, 110, 117, 115, 46, 110, 101, 116),
+                String.fromCharCode(116, 119, 101, 101, 110, 109, 97, 120, 46, 99, 111, 109),
+                String.fromCharCode(116, 119, 101, 101, 110, 108, 105, 116, 101, 46, 99, 111, 109),
+                String.fromCharCode(112, 108, 110, 107, 114, 46, 99, 111),
+                String.fromCharCode(104, 111, 116, 106, 97, 114, 46, 99, 111, 109),
+                String.fromCharCode(106, 115, 102, 105, 100, 100, 108, 101, 46, 110, 101, 116)
+            ], d = c.length; --d > -1;)
+        if (-1 !== a.indexOf(c[d])) return !0;
+    return b && window && window.console && console.log(String.fromCharCode(87, 65, 82, 78, 73, 78, 71, 58, 32, 97, 32, 115, 112, 101, 99, 105, 97, 108, 32, 118, 101, 114, 115, 105, 111, 110, 32, 111, 102, 32)
+     + l +
+     String.fromCharCode(110, 111, 116, 104, 105, 110, 103)), b
+}(window ? window.location.host : ""),
